a826a65d5d6bc6f2db1742944586e3f952f81e1a
fadila08 <nurulfadila08@gmail.com>
2020-06-08 11:49:34 +0700

    acc payable

--- app/Http/Controllers/AccReceivableController.php
+++ app/Http/Controllers/AccReceivableController.php
@@ -17,8 +17,6 @@ use DB;
 
 class AccReceivableController extends Controller
 {
-    private $id_customer = "";
-
     public function __construct()
     {
         $this->middleware('auth');
@@ -26,52 +24,52 @@ class AccReceivableController extends Controller
 
     public function index(Acc_receivable $accReceivable)
     {
-        //cari group idcreditor
-        $data = DB::table('acc_payable')
-        ->join('general_ledgers', 'general_ledgers.id', '=', 'acc_payable.id_transaction')
-        ->groupBy('general_ledgers.id_creditor')
+        //cari group idcustomer
+        $data = DB::table('acc_receivable')
+        ->join('general_ledgers', 'general_ledgers.id', '=', 'acc_receivable.id_transaction')
+        ->join('projects', 'projects.id', '=', 'general_ledgers.id_project')
+        ->join('customers', 'customers.id', '=', 'projects.id_cust')
+        ->groupBy('projects.id_cust')
         ->get();
 
-        //ambil data accpayable
-        $data2 = Acc_payable::with('transaction.creditor')->get();
+        //ambil data accreceivable
+        $data2 = Acc_receivable::with('transaction.project.customer')->get();
 
-        //seleksi data accpayable ke group idcreditor
-        $accPayable = array();
+        //seleksi data accreceivable ke group idcustomer
+        $accReceivable = array();
         foreach ($data as $key => $value) {
             foreach ($data2 as $key2 => $item) {
-                if ($value->id_creditor == $item->transaction->id_creditor) {
-                    $accPayable[$value->id_creditor][] = $item;
+                if ($value->id_cust == $item->transaction->project->id_cust) {
+                    $accReceivable[$value->id_cust][] = $item;
                 }
             }
         }
 
-        // dd( $accPayable);
-       
         $myLog = new myLog;
-        $myLog->go('show','','','acc_payable');
+        $myLog->go('show','','','acc_receivable');
 
-        return view('accPayables.index', ['accPayables' => $accPayable]);
+        return view('accReceivables.index', ['accReceivables' => $accReceivable]);
     }
 
     public function edit($id='')
     {
-        $accPayable = Acc_payable::findOrFail($id);
-        $transaction = General_ledger::where('id','=',$accPayable->id_transaction)->get();
+        $accReceivable = Acc_receivable::findOrFail($id);
+        $transaction = General_ledger::where('id','=',$accReceivable->id_transaction)->get();
 
-        return view('accPayables.edit', compact('accPayable','transaction'));
+        return view('accReceivables.edit', compact('accReceivable','transaction'));
     }
 
-    public function update(AccPayableRequest $request, $id='')
+    public function update(AccReceivableRequest $request, $id='')
     {
-        $accPayable = Acc_payable::findOrFail($id);
+        $accReceivable = Acc_receivable::findOrFail($id);
 
-        $before_value = \json_encode($accPayable);
+        $before_value = \json_encode($accReceivable);
 
-        $accPayable->update($request->all());
+        $accReceivable->update($request->all());
 
         $myLog = new myLog;
-        $myLog->go('update',$before_value,\json_encode($request->all()),'acc_payable');
+        $myLog->go('update',$before_value,\json_encode($request->all()),'acc_receivable');
 
-        return redirect()->route('accPayable.index')->withStatus(__('Due Date successfully updated.'));
+        return redirect()->route('accReceivable.index')->withStatus(__('Due Date successfully updated.'));
     }
 }
