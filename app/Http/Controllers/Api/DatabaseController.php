48d2b6f3e0acb1ad5a88caa31b5381bb0a906bb2
aditnanda <aditya.nanda0030@gmail.com>
2020-06-10 19:43:49 +0700

    generate data database part 2

--- app/Http/Controllers/Api/DatabaseController.php
+++ app/Http/Controllers/Api/DatabaseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Library\MyBackup\Backup_Database;
 
 class DatabaseController extends Controller
 {
@@ -13,84 +14,33 @@ class DatabaseController extends Controller
         $mysqlUserName      = env('DB_USERNAME');
         $mysqlPassword      = env('DB_PASSWORD');
         $DbName             = env('DB_DATABASE');
-        $backup_name        = "mybackup_".date('YmdHis').".fti";
-        $tables             = array(
-            "acc_payable",
-            "acc_receivable",
-            "account_groups",
-            "coas",
-            "creditors",
-            "customers",
-            "failed_jobs",
-            "general_ledgers",
-            "ledgers",
-            "logs",
-            "migrations",
-            "normal_balances",
-            "password_resets",
-            "project_status",
-            "projects",
-            "roles",
-            "services",
-            "taxes",
-            "transaction",
-            "trial_balances",
-            "users"
-        ); //here your tables...
+        $charset = 'utf8';
+        $BACKUP_DIR = '.';
+        $TABLES = '*';
+      
+// Report all errors
+        error_reporting(E_ALL);
+        // Set script max execution time
+        set_time_limit(900); // 15 minutes
 
-        $connect = new \PDO("mysql:host=$mysqlHostName;dbname=$DbName;charset=utf8", "$mysqlUserName", "$mysqlPassword",array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-        $get_all_table_query = "SHOW TABLES";
-        $statement = $connect->prepare($get_all_table_query);
-        $statement->execute();
-        $result = $statement->fetchAll();
-
-
-        $output = '';
-        foreach($tables as $table)
-        {
-         $show_table_query = "SHOW CREATE TABLE " . $table . "";
-         $statement = $connect->prepare($show_table_query);
-         $statement->execute();
-         $show_table_result = $statement->fetchAll();
-
-         foreach($show_table_result as $show_table_row)
-         {
-          $output .= "\n\n" . $show_table_row["Create Table"] . ";\n\n";
-         }
-         $select_query = "SELECT * FROM " . $table . "";
-         $statement = $connect->prepare($select_query);
-         $statement->execute();
-         $total_row = $statement->rowCount();
-
-         for($count=0; $count<$total_row; $count++)
-         {
-          $single_result = $statement->fetch(\PDO::FETCH_ASSOC);
-          $table_column_array = array_keys($single_result);
-          $table_value_array = array_values($single_result);
-          $output .= "\nINSERT INTO $table (";
-          $output .= "" . implode(", ", $table_column_array) . ") VALUES (";
-          $output .= "'" . implode("','", $table_value_array) . "');\n";
-         }
+        if (php_sapi_name() != "cli") {
+            echo '<div style="font-family: monospace;">';
         }
 
-        echo $output;
-        // $file_name = 'backup_on_' . date('y-m-d') . '.fti';
-        // // $file_handle = fopen($file_name, 'w+');
-        // // fwrite($file_handle, $output);
-        // // fclose($file_handle);
-        // // header('Content-Description: File Transfer');
-        // // header('Content-Type: application/octet-stream');
-        // // header('Content-Disposition: attachment; filename=' . basename($file_name));
-        // // header('Content-Transfer-Encoding: binary');
-        // // header('Expires: 0');
-        // // header('Cache-Control: must-revalidate');
-        // //    header('Pragma: public');
-        // //    header('Content-Length: ' . filesize($file_name));
-        // //    ob_clean();
-        // //    flush();
-        // //    readfile($file_name);
-        // //    unlink($file_name);
+        $backupDatabase = new Backup_Database($mysqlHostName, $mysqlUserName, $mysqlPassword, $DbName , $charset);
+        $result = $backupDatabase->backupTables($TABLES, $BACKUP_DIR) ? 'OK' : 'KO';
+        $backupDatabase->obfPrint('Backup result: ' . $result, 1);
 
-        // Storage::disk('local')->put($myfile, $postdata);
+        // Use $output variable for further processing, for example to send it by email
+        $output = $backupDatabase->getOutput();
+
+        if (php_sapi_name() != "cli") {
+            echo '</div>';
+        }
+     
+
+        if ($result == "OK") {
+            echo 'Proses kirim data, mohon ditunggu..';
+        }
     }
 }
