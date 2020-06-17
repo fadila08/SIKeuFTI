9b33ca855f37d34de000d2aa142b0edb6111de63
fadila08 <nurulfadila08@gmail.com>
2020-06-17 16:59:00 +0700

    change stored image name

--- app/Http/Controllers/otherTransactionController.php
+++ app/Http/Controllers/otherTransactionController.php
@@ -39,7 +39,7 @@ class otherTransactionController extends Controller
         $myLog = new myLog;
 
         //insert to jurnal umum
-        $proof = $request->file('upload_proof')->store('proof_transactions'); 
+        $proof = $request->file('upload_proof')->storeAs('proof_transactions', $request->proof_num.'.jpeg'); 
 
         //di exclude dulu upload proofnya
         $data = $request->merge(['nominal' => Crypt::encryptString($request->get('nominal'))])->except('upload_proof');
