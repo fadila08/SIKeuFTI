a8b0d90bd25dae23a5fca8e7f3c8d1e739597fc3
fadila08 <nurulfadila08@gmail.com>
2020-06-17 18:56:59 +0700

    backup and recovery menu

--- app/Http/Controllers/backupRecoveryController.php
+++ app/Http/Controllers/backupRecoveryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\Maintenance;
-
+use App\Http\Library\MyBackup\Restore_Database;
 
 class backupRecoveryController extends Controller
 {
@@ -23,11 +23,41 @@ class backupRecoveryController extends Controller
 
     public function recover()
     {
+        $mysqlHostName      = env('DB_HOST');
+        $mysqlUserName      = env('DB_USERNAME');
+        $mysqlPassword      = env('DB_PASSWORD');
+        $DbName             = env('DB_DATABASE');
+        $charset = 'utf8';
+        $BACKUP_DIR = '';
+        $BACKUP_FILE = 'myphp-backup.sql';
+      
+        // Report all errors
+            /**
+         * Instantiate Restore_Database and perform backup
+         */
+        // Report all errors
+        error_reporting(E_ALL);
+        // Set script max execution time
+        set_time_limit(900); // 15 minutes
+
+        if (php_sapi_name() != "cli") {
+            echo '<div style="font-family: monospace;">';
+        }
+
+        $restoreDatabase = new Restore_Database($mysqlHostName, $mysqlUserName, $mysqlPassword, $DbName , $charset);
+        $result = $restoreDatabase->restoreDb($BACKUP_DIR, $BACKUP_FILE) ? 'OK' : 'KO';
+        $restoreDatabase->obfPrint("Restoration result: ".$result, 1);
+
+        if (php_sapi_name() != "cli") {
+            echo '</div>';
+        }
+
         return redirect()->route('indexBackupRecovery')->withStatus(__('Recovery Database.'));
     }
 
     public function download()
     {
-        return response()->download('public/myphp-backup.sql');
+        $file = public_path()."/myphp-backup.sql";
+        return response()->download($file);
     }
 }
