b025435b718023d1d9c9a64626d548d216382316
fadila08 <nurulfadila08@gmail.com>
2020-06-23 16:26:36 +0700

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Crypt;
 use App\User;
+use App\Cash_flow;
+use App\Profit_loss;
+use App\Acc_payable;
+use App\Acc_receivable;
+use App\Tax;
+use Carbon\Carbon;
 use Auth;
+use DB;
 
 class HomeController extends Controller
 {
@@ -29,14 +38,26 @@ class HomeController extends Controller
     {
         //Super Admin
         if(Auth::user()->id_roles == 1){
-            // $data['user'] = User::with('account')->get();
-            return view('dashboard');
+            $cYear = Carbon::now()->format('Y');             
+            $data['cashFlow'] = Cash_flow::where('period',$cYear)->first();
+            $data['profitLoss'] = Profit_loss::where('period',$cYear)->first();
+            $data['accReceivable'] = Acc_receivable::get();
+            $data['accPayable'] = Acc_payable::get();
+            $data['tax'] = Tax::where('pay_status',0)->get();
+
+            return view('dashboard', $data);
         }
 
         //Admin
         if(Auth::user()->id_roles == 2){
-            // $data['user'] = User::with('account')->where('role','distributor')->get();
-            return view('dashboard');
+            $cYear = Carbon::now()->format('Y');             
+            $data['cashFlow'] = Cash_flow::where('period',$cYear)->first();
+            $data['profitLoss'] = Profit_loss::where('period',$cYear)->first();
+            $data['accReceivable'] = Acc_receivable::get();
+            $data['accPayable'] = Acc_payable::get();
+            $data['tax'] = Tax::where('pay_status',0)->get();
+
+            return view('dashboard', $data);
         }
 
         //direksi
